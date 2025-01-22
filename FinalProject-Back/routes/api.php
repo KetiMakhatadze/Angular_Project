64de09a42a83c958b94a9464fa0d1469e9b2ab42
Keti Makhatadze <maxatadzeketi@gmail.com>
2025-01-22 14:14:59 +0400

    remove comments from api.php

--- FinalProject-Back/routes/api.php
+++ FinalProject-Back/routes/api.php
@@ -2,17 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
 //PUBLIC ROUTES
 Route::post('register', 'App\Http\Controllers\AuthController@register');
 Route::post('login', 'App\Http\Controllers\AuthController@login');
